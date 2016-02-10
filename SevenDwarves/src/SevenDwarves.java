33b8e977c02d61ffa8fd9e54ec25f276a8d183fc
LPLAMY HiSO <LPLAMY HiSO@DESKTOP-4P0G6NG>
2016-02-11 00:03:44 +0700

    ณัฐวุฒิ  คชประภา

---
+++ SevenDwarves/src/SevenDwarves.java
@@ -0,0 +1,155 @@
+
+public class SevenDwarves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10, num2 = 8, num3 = 12, num4 = 40, num5 = 20, num6 = 6, num7 = 4, num8 = 33 , num9 = 94;
+
+		
+		if(num3+num4+num5+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num2");
+		}
+		if(num2+num4+num5+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num3");
+		}
+		if(num3+num2+num5+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num4");
+		}
+		if(num3+num4+num2+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num5");
+		}
+		if(num3+num4+num5+num2+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num6");
+		}
+		if(num3+num4+num5+num6+num2+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num7");
+		}
+		if(num3+num4+num5+num6+num7+num2+num9==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num8");
+		}
+		if(num3+num4+num5+num6+num7+num8+num2==100)
+		{
+			System.out.print("Fake Dwarf is num1 & num9");
+		}
+		if(num1+num4+num5+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num3");
+		}
+		if(num3+num1+num5+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num4");
+		}
+		if(num3+num4+num1+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num5");
+		}
+		if(num3+num4+num5+num1+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num6");
+		}
+		if(num3+num4+num5+num6+num1+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num7");
+		}
+		if(num3+num4+num5+num6+num7+num1+num9==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num8");
+		}
+		if(num3+num4+num5+num6+num7+num8+num1==100)
+		{
+			System.out.print("Fake Dwarf is num2 & num9");
+		}
+		if(num1+num2+num5+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num3 & num4");
+		}
+		if(num1+num4+num2+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num3 & num5");
+		}
+		if(num1+num4+num5+num2+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num3 & num6");
+		}
+		if(num1+num4+num5+num6+num2+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num3 & num7");
+		}
+		if(num1+num4+num5+num6+num7+num2+num9==100)
+		{
+			System.out.print("Fake Dwarf is num3 & num8");
+		}
+		if(num1+num4+num5+num6+num7+num8+num2==100)
+		{
+			System.out.print("Fake Dwarf is num3 & num9");
+		}
+		if(num3+num1+num2+num6+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num4 & num5");
+		}
+		if(num3+num1+num5+num2+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num4 & num6");
+		}
+		if(num3+num1+num5+num6+num2+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num4 & num7");
+		}
+		if(num3+num1+num5+num6+num7+num2+num9==100)
+		{
+			System.out.print("Fake Dwarf is num4 & num8");
+		}
+		if(num3+num1+num5+num6+num7+num8+num2==100)
+		{
+			System.out.print("Fake Dwarf is num4 & num9");
+		}
+		if(num3+num4+num1+num2+num7+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num5 & num6");
+		}
+		if(num3+num4+num1+num6+num2+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num5 & num7");
+		}
+		if(num3+num4+num1+num6+num7+num2+num9==100)
+		{
+			System.out.print("Fake Dwarf is num5 & num8");
+		}
+		if(num3+num4+num1+num6+num7+num8+num2==100)
+		{
+			System.out.print("Fake Dwarf is num5 & num9");
+		}
+		if(num3+num4+num5+num1+num2+num8+num9==100)
+		{
+			System.out.print("Fake Dwarf is num6 & num7");
+		}
+		if(num3+num4+num5+num1+num7+num2+num9==100)
+		{
+			System.out.print("Fake Dwarf is num6 & num8");
+		}
+		if(num3+num4+num5+num1+num7+num8+num2==100)
+		{
+			System.out.print("Fake Dwarf is num6 & num9");
+		}
+		if(num3+num4+num5+num6+num1+num2+num9==100)
+		{
+			System.out.print("Fake Dwarf is num7 & num8");
+		}
+		if(num3+num4+num5+num6+num1+num8+num2==100)
+		{
+			System.out.print("Fake Dwarf is num7 & num9");
+		}
+		if(num3+num4+num5+num6+num7+num1+num2==100)
+		{
+			System.out.print("Fake Dwarf is num8 & num9");
+		}
+	}
+
+}
